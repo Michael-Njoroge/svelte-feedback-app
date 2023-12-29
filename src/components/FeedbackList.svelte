4fd81c9a7ab104af4f0343e0002f1a5af5cef86d
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-29 20:13:52 +0300

    finish up on feedback item

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,7 +1,11 @@
 <script>
+import FeedbackItem from "./FeedbackItem.svelte";
+
     export let feedback = []
 
     console.log(feedback)
 </script>
 
-Feedback
+{#each feedback as fb (fb.id) }
+<FeedbackItem item = {fb}/>
+{/each}
