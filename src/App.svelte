53c414283211a48e491b281dcd67e5380010b582
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-29 21:19:37 +0300

    complete display of feedback stats

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,27 @@
 <script>
   import FeedbackList from "./components/FeedbackList.svelte";
+  import FeedbackStats from "./components/FeedbackStats.svelte";
  
  let feedback = [
   {
     id: '1',
-    rating: '10',
+    rating: 10,
     text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis temporibus quo qui ut? Sit, alias. Nostrum, enim tempora. Aperiam harum facere iste delectus! Consequuntur alias repellendus tempore vitae nostrum esse'
   },
   {
     id: '2',
-    rating: '9',
+    rating: 9,
     text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis temporibus quo qui ut? Sit, alias. Nostrum, enim tempora. Aperiam harum facere iste delectus! Consequuntur alias repellendus tempore vitae nostrum esse'
   },
   {
     id: '3',
-    rating: '8',
+    rating: 8,
     text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis temporibus quo qui ut? Sit, alias. Nostrum, enim tempora. Aperiam harum facere iste delectus! Consequuntur alias repellendus tempore vitae nostrum esse'
   },
  ]
+
+ $:count = feedback.length
+ $:average = feedback.reduce((a,{rating}) => a + rating, 0) / feedback.length
 
  const deleteFeedback = (e) => {
   const itemId = e.detail
@@ -27,6 +31,7 @@
 </script>
 
 <main class="container">
+  <FeedbackStats {count} {average}/>
   <FeedbackList {feedback} on:delete_feedback = {deleteFeedback}/>
 </main>
  
