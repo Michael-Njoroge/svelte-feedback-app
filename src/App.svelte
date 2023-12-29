86de9cb0547d7b9754f3d7d1b73daa6e74083f92
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-29 19:40:16 +0300

    getting started with svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,25 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+let firstname = 'Michael';
+let lastName = 'Njoroge';
+let color = 'blue';
+
+const toggle = ()  => {
+  color = color === 'blue' ? 'red' : 'blue'
+}
+
+$:name = firstname+ ' ' + lastName
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <h1 style="color:{color}">Hello {name}!</h1>
+  <button on:click={toggle}>click</button>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  main{
+    text-align: center;
+    padding: 1rem;
+    /* max-width: 240px; */
+    margin: 0 auto;
   }
 </style>
