8dd7c0f8d58b2903cde93f765f175e4b5214a8a5
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-30 03:53:19 +0300

    in and out animation

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,11 +1,13 @@
 <script>
 import FeedbackItem from "./FeedbackItem.svelte";
+import { fade,scale } from "svelte/transition";
 
     export let feedback = []
-
-    console.log(feedback)
+ 
 </script>
 
 {#each feedback as fb (fb.id) }
+<div in:scale out:fade={{duration:500}}>
 <FeedbackItem item = {fb} on:delete_feedback/>
+</div>
 {/each}
